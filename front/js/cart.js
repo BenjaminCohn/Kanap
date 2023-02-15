ebdc30e8c75f81ed75167213aeb6165bc650ce73
BenjaminCohn <Benjamin.cohen@eemi.com>
2023-02-15 13:04:53 +0100

    commencement de cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,9 +1,9 @@
 //_________________Modification de la balise title du navigateur cart --> Page Panier (plus compréhensible par l'utilisateur)_____________________
-documenter . title  =  "Page Panier" ;
+document . title  =  "Page Panier" ;
 
 //______Déclaration de la variable "productRegisterInLocalStorage" dans laquelle on met les clés et les valeurs qui sont dans le local Storage______
 //----------------JSON.parse c'est pour convertir les données au format JSON qui sont dans le localStorage en objet javascript---------- ----------
-laissez  productRegisterInLocalStorage  =  JSON . parse ( localStorage . getItem ( "produit" ) ) ;
+let  productRegisterInLocalStorage  =  JSON . parse ( localStorage . getItem ( "produit" ) ) ;
     //console.log(productRegisterInLocalStorage);
 //--------------------Sélection de la balise de la page product.html dans laquel on va insérer les produits et leurs infos-------- -----------------
 const  produitsPositionHtml  =  document . getElementById ( "cart__items" ) ;
